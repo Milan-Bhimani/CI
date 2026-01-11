44a12c8deb59fbbe3d9cb13fab1e80f4f8040e3a
Milan BHimani <milanhbhimani@gmail.com>
2026-01-11 15:00:45 +0530

    Trying to add dynamic input

--- sum.js
+++ sum.js
@@ -1,5 +1,15 @@
-const a = parseInt(process.argv[2])
-const b = parseInt(process.argv[3])
+// for terminal input
+// const a = parseInt(process.argv[2])
+// const b = parseInt(process.argv[3])
+
+// for input of .env file
+
+const a = parseInt(process.env.A)
+const b = parseInt(process.env.B)
+
+if (isNaN(a) || isNaN(b)) {
+  throw new Error("Environment variables A and B must be numbers");
+}
 
 function sum(x,y){
     return x + y;
